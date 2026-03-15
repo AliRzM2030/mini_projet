55f27a0bc36a2647fc9afe0fd8a42a85c8cb0ed1
AliRzM2030 <aliyane.benchehida@efrei.net>
2026-03-15 02:20:26 +0100

    Add initial data insertions for game entities

---
+++ 3_insertion.sql
@@ -0,0 +1,24 @@
+INSERT INTO arene VALUES (1,'Arène Gobeline',0);
+INSERT INTO arene VALUES (2,'Vallée des sorts',600);
+
+INSERT INTO clan VALUES (1,'Royaux FR','Clan français',50);
+
+INSERT INTO joueur VALUES (1,'AliMaster',40,5500,120000,300,1);
+INSERT INTO joueur VALUES (2,'AnysPro',38,5200,90000,200,1);
+
+INSERT INTO carte VALUES (1,'Chevalier','troupe','commune',3);
+INSERT INTO carte VALUES (2,'Archers','troupe','commune',3);
+
+INSERT INTO combat VALUES (1,'2026-03-01',1);
+
+INSERT INTO deck VALUES (1,'Deck Ali',1);
+
+INSERT INTO recompense VALUES (1,500,10,'2026-03-01',1,1);
+
+INSERT INTO posseder VALUES (1,1,12,200);
+
+INSERT INTO participer VALUES (1,1,'attaque','victoire',30);
+
+INSERT INTO composer VALUES (1,1,1);
+
+INSERT INTO recompense_carte VALUES (1,2,5);
